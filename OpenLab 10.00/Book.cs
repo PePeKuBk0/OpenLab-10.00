fdabb69d6b16cd1f46de7c85e63f1096b5a1ec8a
Jakub Venetianer <jakub.venetianer@gmail.com>
2020-12-13 17:07:45 +0100

    Add project files.

---
+++ OpenLab 10.00/Book.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open_Lab_10._00
+{
+    class Book
+    {
+        public static List<string> categoryList = new List<string>() { "detské", "romantické", "náučné", "sci-fi", "dobrodružné" };
+
+        private string Title;
+        private int Pages;
+        private string Category;
+        private string Author;
+        private int ReleaseDate;
+
+
+        public string author { get; set; }
+
+        public string category
+        {
+
+
+            set => Category = value;
+        }
+
+        public int pages
+        {
+            get => this.Pages;
+            set
+            {
+                if (value < 1)
+                {
+                    Pages = 1;
+                }
+                else
+                {
+                    Pages = value;
+                }
+            }
+        }
+
+
+        public int releaseDate
+        {
+            get => this.ReleaseDate;
+            set
+            {
+                if (value < 1450 || value > 2020)
+                {
+                    ReleaseDate = -1;
+                }
+                else
+                {
+                    ReleaseDate = value;
+                }
+            }
+        }
+
+        public string title
+        { get; set; }
+
+        public Book()
+        {
+            title = "-1";
+            pages = -1;
+            category = "-1";
+            author = "-1";
+            releaseDate = -1;
+        }
+
+        public Book(string title, int pages)
+        {
+            this.title = title;
+            this.pages = pages;
+            category = "-1";
+            author = "-1";
+            releaseDate = -1;
+        }
+
+        public Book(string title, int pages, string category, string author, int releaseDate)
+        {
+            this.title = title;
+            this.pages = pages;
+            this.category = category;
+            this.author = author;
+            this.releaseDate = releaseDate;
+        }
+
+        public override string ToString()
+        {
+            return $"Title:{this.title}, Pages:{this.Pages}, Category:{this.Category}, Author:{this.author}, RealeaseDate:{this.ReleaseDate}";
+
+        }
+
+    }
+}
---
+++ OpenLab 10.00/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Open_Lab_10._00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book LOTR = new Book();
+
+            LOTR.author = "Jakub Venetianer";
+            Console.WriteLine(LOTR.author);
+
+            LOTR.category = "Turorial";
+            Console.WriteLine(Book.categoryList[0]);
+
+            LOTR.pages += 5;
+            Console.WriteLine(LOTR.pages);
+
+            LOTR.releaseDate += 1400;
+            Console.WriteLine(LOTR.releaseDate);
+
+            LOTR.title = "Hobit";
+            Console.WriteLine(LOTR.title);
+
+
+
+            Book LOTR2 = new Book();
+
+
+            Book LOTR3 = new Book("Kravina", 60);
+
+
+            Book HOBIT = new Book("Somarina", 88, Book.categoryList[0], "Botto", 2000);
+
+            Console.WriteLine("");
+            Console.WriteLine("LOTR2: " + LOTR2.ToString());
+            Console.WriteLine("");
+            Console.WriteLine("LOTR3: " + LOTR3.ToString());
+            Console.WriteLine("");
+            Console.WriteLine("HOBIT: " + HOBIT.ToString());
+
+
+        }
+
+    }
+}
